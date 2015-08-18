c7bb0c1b88260d8bae6f103085fe28f12cbe406a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-18 15:15:02 +0200

    rename epsilon -> mu

--- setupExpCode.m
+++ setupExpCode.m
@@ -5,7 +5,7 @@ expCreate('rwcInstruments');
 rwcInstruments('addStep','scattering');
 rwcInstruments('addFactor',{'arch',{'plain'}});
 rwcInstruments('addFactor',{'Q',{'8','16'}});
-rwcInstruments('addFactor',{'epsilon',{'1e-2','1','1e2'}})
+rwcInstruments('addFactor',{'mu',{'1e-2','1','1e2'}})
 
 rwcScattering('addStep','pca');
 rwcScattering('addFactor',{'rank',{'50','100'}})

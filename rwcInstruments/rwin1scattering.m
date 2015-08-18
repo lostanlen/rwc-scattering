6fbf76eb5b6d6b82da1fc931787ffa8ecf8ea77d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-18 15:23:10 +0200

    specify scattering options

--- rwcInstruments/rwin1scattering.m
+++ rwcInstruments/rwin1scattering.m
@@ -12,6 +12,20 @@ function [config, store, obs] = rwin1scattering(config, setting, data)
                                                                                      
 % Set behavior for debug mode                                                        
 if nargin==0, rwcInstruments('do', 1, 'mask', {}); return; else store=[]; obs=[]; end
-                                                                                     
-% imported data                                                                      
-data                                                                                 
+
+opts{1}.time.T = 16384; % 370 ms @ 44,1 kHz
+opts{1}.time.max_Q = Q;
+opts{1}.time.max_scale = 4410.0; % 100 ms @ 44,1 kHz
+opts{1}.time.gamma_bounds = [1 9*Q]; % 9 octaves from 43 Hz to 22,050 kHz
+
+opts{1}.nonlinearity.type = 'uniform_log';
+opts{1}.nonlinearity.denominator = mu;
+
+opts{2}.time.handle = @gammatone_1d;
+opts{2}.time.sibling_mask_factor = 2.0;
+opts{2}.time.U_log2_oversampling = 2;
+
+archs = sc_setup(opts);
+
+
+

d6d27ed1f80b742b3ce1d9564d13b9f9d628f33e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-19 17:29:20 +0200

    rename rwcScattering -> rwcInstruments

--- setupExpCode.m
+++ setupExpCode.m
@@ -8,5 +8,5 @@ rwcInstruments('addFactor',{'arch',{'plain'}});
 rwcInstruments('addFactor',{'Q',{'8','16'}});
 rwcInstruments('addFactor',{'mu',{'1e-2','1','1e2'}})
 
-rwcScattering('addStep','pca');
-rwcScattering('addFactor',{'rank',{'50','100'}, '2'})
+rwcInstruments('addStep','pca');
+rwcInstruments('addFactor',{'rank',{'50','100'}, '2'})

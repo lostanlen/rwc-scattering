3a576043a5227e2f5369e83cbb8d997a21ffdd0d
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-19 18:44:41 +0200

    move code to experiment folder

--- setupExpCode.m
+++ setupExpCode.m
@@ -22,4 +22,7 @@ rwcInstruments('addStep','scattering');
 rwcInstruments('addFactor',{'arch',{'plain'}});
 rwcInstruments('addFactor',{'mu',{'1e-2','1','1e2'}});
 rwcInstruments('addFactor',{'Q',{'8','16'}});
-rwcInstruments('addFactor', {'batch_id','1:45'})
+rwcInstruments('addFactor', {'batch_id','1:45'});
+
+% Move code to experiment folder
+system('rsync -a rwin1scattering.m ./rwcInstruments/')

7c217d60b26825b9affdd94cea842e8e365e78ee
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-08-19 17:56:34 +0200

    semicolons avoid spurious outputs

--- setupExpCode.m
+++ setupExpCode.m
@@ -1,24 +1,24 @@
 % Pull latest master of scattering.m
-cd('~/scattering.m')
-system('git pull origin master')
-cd('~/rwc-scattering')
+cd('~/scattering.m');
+system('git pull origin master');
+cd('~/rwc-scattering');
 
 % Pull latest master of rwcInstrument project
-system('git pull origin master')
+system('git pull origin master');
 
 % Move .expCode hidden folder to home
-movefile('.expCode', '~/')
+movefile('.expCode', '~/');
 
 % Add expCode to path
 addpath(genpath('~/expCode'));
-addpath(genpath('~/scattering.m'))
+addpath(genpath('~/scattering.m'));
 
 % Create experiment
 expCreate('rwcInstruments');
 rwcInstruments('addStep','scattering');
 rwcInstruments('addFactor',{'arch',{'plain'}});
 rwcInstruments('addFactor',{'Q',{'8','16'}});
-rwcInstruments('addFactor',{'mu',{'1e-2','1','1e2'}})
+rwcInstruments('addFactor',{'mu',{'1e-2','1','1e2'}});
 
 rwcInstruments('addStep','pca');
-rwcInstruments('addFactor',{'rank',{'50','100'}, '2'})
+rwcInstruments('addFactor',{'rank',{'50','100'}, '2'});

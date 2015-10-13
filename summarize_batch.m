c3344f5b2784395bcbee85c6b1cd0530943f8bd2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-13 16:21:18 +0200

    define max-pooling in summarize_batch

--- summarize_batch.m
+++ summarize_batch.m
@@ -1,11 +1,15 @@
 function summarized_batch = summarize_batch(batch, summarization_str)
-nFiles = length(batch);
+nFiles_in_batch = length(batch);
 summarized_batch = batch;
 switch summarization_str
     case 'none'
     case 'mean'
-        for file_id = 1:nFiles
-            summarized_batch(file_id).data = mean(batch(file_id).data);
+        for file_id = 1:nFiles_in_batch
+            summarized_batch(file_id).data = mean(batch(file_id).data, 2);
+        end
+    case 'max'
+        for file_id = 1:nFiles_in_batch
+            summarized_batch(file_id).data = max(batch(file_id).data, [], 2);
         end
 end
 end

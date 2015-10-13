d63d9cc92fc940a0dcd8c1113e49c4087f0ce369
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-13 10:23:06 +0200

    whitespace in compute_pca

--- compute_pca.m
+++ compute_pca.m
@@ -11,9 +11,9 @@ X = [features.data].';
 % Generate savefile name
 switch setting.arch
     case 'plain'
-        Q_str = num2str(setting.Q,'%1.2d');
-        mu_str = num2str(setting.mu,'%1.0e');
-        savefile_name = ['rwcplainPCA_Q',Q_str,'_mu',mu_str];
+        Q_str = num2str(setting.Q, '%1.2d');
+        mu_str = num2str(setting.mu, '%1.0e');
+        savefile_name = ['rwcplainPCA_Q', Q_str, '_mu', mu_str];
 end
 
 % Get host name
@@ -25,15 +25,15 @@ host = char(host); % convert to MATLAB char array
 date = datestr(now());
 
 % Save
-if ~exist('PCAs','dir')
+if ~exist('PCAs', 'dir')
     mkdir('PCAs');
 end
-savefile_path = ['PCAs/',savefile_name];
+savefile_path = ['PCAs/', savefile_name];
 save(savefile_path, 'coeff', 'Xmean', 'explained');
 
 % Print termination message
 disp('--------------------------------------------------------------------------------');
 disp(['Finished PCA on host ', host,' at ', date,' with settings:']);
 disp(setting);
-disp(['Elapsed time is ', elapsed_str ' seconds.']);
+disp(['Elapsed time is ', elapsed_str, ' seconds.']);
 end

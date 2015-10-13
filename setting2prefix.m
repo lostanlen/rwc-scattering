9f3630d71b9cacb85d5e34f2e699c8029fbc4222
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-13 13:18:34 +0200

    define mu_str in setting2prefix

--- setting2prefix.m
+++ setting2prefix.m
@@ -4,6 +4,7 @@ if ~strcmp(setting.arch, 'mfcc')
     prefix = [prefix, '_Q', num2str(setting.Q, '%1.2d')];
     prefix = [prefix, '_wavelet', setting.wavelet];
     if isfield(setting, 'mu')
+        mu_str = num2str(setting.mu,'%1.0e');
         prefix = [prefix, '_mu', mu_str];
     end
 end

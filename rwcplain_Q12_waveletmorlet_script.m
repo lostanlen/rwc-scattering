314b105151911ea228299eeeb88d0e2bd422fc05
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-13 16:20:06 +0200

    update rwcplain_Q12_waveletmorlet_script.m

--- rwcplain_Q12_waveletmorlet_script.m
+++ rwcplain_Q12_waveletmorlet_script.m
@@ -2,7 +2,11 @@ setting.arch = 'plain';
 setting.Q = 12;
 setting.wavelet = 'morlet';
 
-nBatches = 45;
-for batch_id = 1:nBatches
-    compute_batch(batch_id, setting);
-end
+%% This loop in computed in the cluster
+% nBatches = 45;
+% for batch_id = 1:nBatches
+%     compute_batch(batch_id, setting);
+% end
+
+%%
+features = load_features(setting);

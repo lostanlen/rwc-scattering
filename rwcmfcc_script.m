4a34306bcef563be159c01bd7e4703143a7373c5
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-10-13 13:15:17 +0200

    update rwcmfcc_script

--- rwcmfcc_script.m
+++ rwcmfcc_script.m
@@ -1,6 +1,13 @@
 setting.arch = 'mfcc';
 
-nBatches = 45;
-for batch_id = 1:nBatches
-    compute_batch(batch_id, setting);
-end
+%% This loop in computed in the cluster
+% nBatches = 45;
+% for batch_id = 1:nBatches
+%     compute_batch(batch_id, setting);
+% end
+
+%%
+features = load_features(setting);
+
+%%
+average_distances = compute_average_distances(features);

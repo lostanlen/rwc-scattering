a098822f38a6f79ec57c790e2e187b8a21d65402
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-20 17:06:01 +0100

    remove wavelet prefix, gammatone will be the default now

--- setting2prefix.m
+++ setting2prefix.m
@@ -2,7 +2,6 @@ function prefix = setting2prefix(setting)
 prefix = ['rwc', setting.arch];
 if ~strcmp(setting.arch, 'mfcc')
     prefix = [prefix, '_Q', num2str(setting.Q, '%1.2d')];
-    prefix = [prefix, '_wavelet', setting.wavelet];
     if isfield(setting, 'mu')
         mu_str = num2str(setting.mu,'%1.0e');
         prefix = [prefix, '_mu', mu_str];

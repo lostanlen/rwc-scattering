2adc6f0a6bcf7b10bfbbea50e6056d42fdc16577
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-21 17:53:37 +0100

    finish writing rwcplain_Q12_mu1em2_script

--- rwcplain_Q12_mu1em2_script.m
+++ rwcplain_Q12_mu1em2_script.m
@@ -6,3 +6,9 @@ nBatches = 45;
 for batch_id = 1:nBatches
     compute_batch(batch_id, setting);
 end
+
+%%
+features = load_features(setting, 'max');
+
+%%
+summary = compute_average_distances(setting, features)

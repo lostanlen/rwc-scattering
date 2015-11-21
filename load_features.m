5b08f757428f81eca85d6adb348fa3bad4ffe196
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-21 20:53:51 +0100

    better pretty-printing of batch loading

--- load_features.m
+++ load_features.m
@@ -5,7 +5,7 @@ nBatches = 45;
 batch_features = cell(nBatches,1);
 % Load batches
 for batch_id = 1:nBatches
-    disp(['loading batch #', int2str(batch_id)])
+    disp(['loading batch #', num2str(batch_id, '%0.2d')])
     batch_id_str = num2str(batch_id, '%1.2d');
     file_name = ['batch', batch_id_str];
     file_path = [prefix, '/', file_name];

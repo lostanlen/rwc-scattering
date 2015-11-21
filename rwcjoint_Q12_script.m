06924f3fffaea826ac854be7a9e337c4acc6992e
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-21 17:52:19 +0100

    update rwcjoint_Q12_script

--- rwcjoint_Q12_script.m
+++ rwcjoint_Q12_script.m
@@ -5,3 +5,9 @@ nBatches = 45;
 for batch_id = 1:nBatches
     compute_batch(batch_id, setting);
 end
+
+%%
+features = load_features(setting, 'max');
+
+%%
+summary = compute_average_distances(setting, features)

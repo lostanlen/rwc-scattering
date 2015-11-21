e62e24fdd93dcf7ea8c4f95be9ad08c2f0419bd9
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-22 00:16:38 +0100

    write rwcspiral_Q12_script

---
+++ rwcspiral_Q12_script.m
@@ -0,0 +1,13 @@
+setting.arch = 'spiral';
+setting.Q = 12;
+
+nBatches = 45;
+for batch_id = 1:nBatches
+    compute_batch(batch_id, setting);
+end
+
+%%
+features = load_features(setting, 'max');
+
+%%
+summary = compute_average_distances(setting, features)

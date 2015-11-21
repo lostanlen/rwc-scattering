e4a0b72869153281b56f047037ccc5d0628cb8db
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-21 17:53:13 +0100

    update setup_scattering, blurred invariants with oversampling

--- setup_scattering.m
+++ setup_scattering.m
@@ -2,7 +2,8 @@ function archs = setup_scattering(setting)
 % Build scattering filter banks
 % First order
 % 370 ms @ 44,1 kHz
-opts{1}.time.T = 16384;
+T = 32768;
+opts{1}.time.T = T;
 opts{1}.time.max_Q = setting.Q;
 % 8 octaves
 opts{1}.time.gamma_bounds = [1 8*setting.Q];
@@ -18,9 +19,10 @@ else
     opts{1}.nonlinarity.name = 'modulus';
 end
 % Second order
-opts{2}.banks.time.handle = @gammatone_1d;
-opts{2}.banks.time.sibling_mask_factor = 2.0;
-opts{2}.banks.time.U_log2_oversampling = 2;
+opts{2}.time.handle = @gammatone_1d;
+opts{2}.time.sibling_mask_factor = 2.0;
+opts{2}.time.U_log2_oversampling = 2;
+opts{2}.time.S_log2_oversampling = 2;
 % Joint scattering
 if strcmp(setting.arch, 'joint')
     opts{2}.banks.gamma = struct();
@@ -30,12 +32,17 @@ if strcmp(setting.arch, 'spiral')
     opts{2}.banks.gamma.handle = @morlet_1d;
     opts{2}.banks.j.handle= @finitediff_1d;
 end
-% Max-pooling
-opts{2}.invariants.time.invariance = 'maxpooled';
+% Second-order blurring
+opts{2}.invariants.time.invariance = 'blurred';
+opts{2}.invariants.time.T = T;
+opts{2}.invariants.time.S_log2_oversampling = 2;
 % Second nonlinearity
 opts{2}.nonlinearity.name = 'modulus';
-% Frequency transposition invariance
-opts{3}.invariants.time.invariance = 'maxpooled';
+% Third-order blurring
+opts{3}.invariants.time.invariance = 'blurred';
+opts{3}.invariants.time.T = T;
+opts{3}.invariants.time.S_log2_oversampling = 2;
+% Third-order frequency transposition invariance
 opts{3}.invariants.gamma.invariance = 'summed';
 % Setup architectures
 archs = sc_setup(opts);

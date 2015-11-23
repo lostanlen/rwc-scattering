3df7853d56b0c91ead34215feda0649531c5bfbf
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-23 11:07:28 +0100

    uncomment rwcmfcc script

--- rwcmfcc_script.m
+++ rwcmfcc_script.m
@@ -1,10 +1,10 @@
 setting.arch = 'mfcc';
 
-%% This loop in computed in the cluster
-% nBatches = 45;
-% for batch_id = 1:nBatches
-%     compute_batch(batch_id, setting);
-% end
+%  This loop in computed in the cluster
+nBatches = 45;
+for batch_id = 1:nBatches
+    compute_batch(batch_id, setting);
+end
 
 %%
 features = load_features(setting, 'max');

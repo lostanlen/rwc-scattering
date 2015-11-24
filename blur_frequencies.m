e2c47f5ca29c4fdc62e6558c896aeac321b4d8d2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-24 11:19:56 +0100

    write blurfreq_plainS2

--- blur_frequencies.m
+++ blur_frequencies.m
@@ -1,18 +1,17 @@
-function S = blur_frequencies(S, F)
+function S = blurfreq_plainS2(S, F)
 %% Set options
-S = rwcbatch(1).S;
-opt.T = 8;
-opt.size = 2^nextpow2(size(S{1+1}.data,2));
+opt.T = F;
+opt.size = 8 * 2^nextpow2(size(S{1+1}.data,2));
 opt.has_multiple_support = true;
 opt.key.time{1}.gamma{1} = [];
 opt.subscripts = 2;
 invariant.spec = fill_invariant_spec(opt);
 invariant.behavior = fill_invariant_behavior(opt);
 
-% Setup bank
+%% Setup bank
 invariant = setup_invariant(invariant);
 
-% First layer
+%% First layer
 % Pad
 padding = zeros(size(S{1+1}.data, 1), opt.size - size(S{1+1}.data, 2));
 unpadded_proportion = size(S{1+1}.data, 2) / opt.size;
@@ -23,20 +22,30 @@ S{1+1} = perform_ft(S{1+1}, invariant.behavior.key);
 S{1+1} = blur_Y(S{1+1}, invariant);
 
 % Unpad
-nUnpadded_frequencies = round(unpadded_proportion * size(S{1+1}.data, 2));
+nUnpadded_frequencies = ceil(unpadded_proportion * size(S{1+1}.data, 2));
 S{1+1}.data = S{1+1}.data(:, 1:nUnpadded_frequencies);
 
 %% Second layer
-% Loop across lambda_2
+% Pad
 nLambda2s = length(S{1+2}.data);
+unpadded_proportions = zeros(nLambda2s, 1);
 for lambda2_index = 1:nLambda2s
-    %%
-    lambda2_index = 1
     sub_S2 = S{1+2}.data{lambda2_index};
-    % Pad
-    padded_size = 2^nextpow2(size(sub_S2, 2));
+    padded_size = 8 * 2^nextpow2(size(sub_S2, 2));
     padding = zeros(size(sub_S2, 1), padded_size - size(sub_S2, 2));
-    sub_S2 = cat(2, sub_S2, padding);
-    % Blur
+    unpadded_proportions(lambda2_index) = size(sub_S2, 2) / padded_size;
+    S{1+2}.data{lambda2_index} = cat(2, sub_S2, padding);
+end
+
+% Blur
+S{1+2} = perform_ft(S{1+2}, invariant.behavior.key);
+S{1+2} = blur_Y(S{1+2}, invariant);
+
+% Unpad
+for lambda2_index = 1:nLambda2s
+    sub_S2 = S{1+2}.data{lambda2_index};
+    unpadded_proportion = unpadded_proportions(lambda2_index);
+    nUnpadded_frequencies = ceil(unpadded_proportion * size(sub_S2, 2));
+    S{1+2}.data{lambda2_index} = real(sub_S2(:, 1:nUnpadded_frequencies));
 end
 end

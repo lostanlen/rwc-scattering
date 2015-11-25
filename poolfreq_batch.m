9cacd632f08ecf8c6bcfc549fcb0bbec0ebf554b
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-25 13:30:26 +0100

    write poolfreq_batch

---
+++ poolfreq_batch.m
@@ -0,0 +1,68 @@
+function poolfreq_batch(batch_id, setting, B)
+%% Generate prefix string
+prefix = setting2prefix(setting);
+
+%% Dispatch poolfreq according to architecture
+switch setting.arch
+    case 'plain'
+        poolfreq_handle = @poolfreq_plainS2;
+    case 'joint'
+        poolfreq_handle = @poolfreq_jointS2;
+    case 'spiral'
+        poolfreq_handle = @poolfreq_spiralS2;
+end
+
+%% Load batch
+batch_id_str = num2str(batch_id, '%1.2d');
+file_name = ['batch', batch_id_str];
+file_path = [prefix, '/', file_name];
+load(file_path);
+
+%% Loop over files
+newbatch = rwcbatch;
+nFiles = length(newbatch);
+tic();
+parfor file_index = 1:nFiles
+    S = newbatch(file_index).S;
+    S = poolfreq_handle(S, B);
+    S1_mat = format_layer(S{1+1}, 1);
+    S2_mat = format_layer(S{1+2}, 1);
+    newbatch(file_index).data = cat(2, S1_mat, S2_mat).';
+    newbatch(file_index).S = S;
+end
+rwcbatch = newbatch;
+
+%% Measure elapsed time
+elapsed = toc();
+elapsed_str = num2str(elapsed, '%2.0f');
+
+%% Get host name
+pcinfo = java.net.InetAddress.getLocalHost();
+host = pcinfo.getHostName(); % class is java.lang.String
+host = char(host); % convert to MATLAB char array
+
+%% Get date
+date = datestr(now());
+
+%% Generate output file path
+newsetting = setting;
+newsetting.F = B;
+newprefix = setting2prefix(newsetting);
+
+%% Save
+batch_id_str = num2str(batch_id, '%1.2d');
+savefile_name = ['batch', batch_id_str];
+if ~exist(newprefix,'dir')
+    mkdir(newprefix);
+end
+savefile_path = [newprefix, '/', savefile_name];
+save(savefile_path, 'rwcbatch', 'setting', 'host', 'elapsed', 'date');
+
+%% Print termination message
+% Print termination message
+disp('--------------------------------------------------------------------------------');
+disp(['Finished batch ', batch_id_str, ' on host ', host, ...
+    ' at ', date,' with settings:']);
+disp(newsetting);
+disp(['Elapsed time is ', elapsed_str ' seconds.']);
+end

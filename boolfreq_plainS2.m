61dd3dcc6207850fe577805da0cb7329b5526ad7
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-25 16:36:21 +0100

    write boolfreq_plainS2

---
+++ boolfreq_plainS2.m
@@ -0,0 +1,40 @@
+function S = poolfreq_plainS2(S, B)
+%% First layer
+% Read
+S1 = S{1+1}.data;
+
+% Pad
+nFrequencies_in = size(S1, 2);
+nFrequencies_out = ceil(nFrequencies_in / B);
+padding_length = nFrequencies_out * B - nFrequencies_in;
+padding = zeros(size(S1, 1), padding_length);
+S1 = cat(2, S1, padding);
+
+% Reshape
+S1 = reshape(S1, size(S1, 1), B, nFrequencies_out);
+
+% Max-pool
+S1 = max(S1, [], 3);
+
+% Write
+S{1+1}.data = squeeze(S1);
+
+%% Second layer
+nNodes = length(S{1+2}.data);
+for node_index = 1:nNodes
+    % Read
+    S2 = S{1+2}.data{node_index};
+    % Pad
+    nFrequencies_in = size(S2, 2);
+    nFrequencies_out = ceil(nFrequencies_in / B);
+    padding_length = nFrequencies_out * B - nFrequencies_in;
+    padding = zeros(size(S2, 1), padding_length);
+    S2 = cat(2, S2, padding);
+    % Reshape
+    S2 = reshape(S2, size(S2, 1), B, nFrequencies_out);  
+    % Max-pool
+    S2 = max(S2, [], 3);
+    % Write
+    S{1+2}.data = squeeze(S2);
+end
+end

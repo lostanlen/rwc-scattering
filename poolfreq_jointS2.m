e7c5c2c6899cabcace8448a6d699fade3341e32f
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-26 10:12:03 +0100

    write poolfreq_jointS2

---
+++ poolfreq_jointS2.m
@@ -0,0 +1,52 @@
+function S = poolfreq_jointS2(S, B)
+%% First layer
+% Read
+S1 = S{1+1}.data;
+
+% Pad
+nFrequencies_in = size(S1, 2);
+nFrequencies_out = ceil(nFrequencies_in / B);
+padding_length = nFrequencies_out * B - nFrequencies_in;
+padding = zeros(size(S1, 1), padding_length);
+S1 = cat(2, S1, padding);
+
+% Reshape
+S1 = reshape(S1, size(S1, 1), B, nFrequencies_out);
+
+% Max-pool
+S1 = max(S1, [], 2);
+
+% Write
+S{1+1}.data = squeeze(S1);
+
+% Update ranges
+S{1+1}.ranges{1+0}(2,2) = B;
+
+%% Second layer
+nBlobs = length(S{1+2}.data);
+for blob_index = 1:nBlobs
+    % Read blob
+    blob = S{1+2}.data{blob_index};
+    nNodes = length(blob);
+    for node_index = 1:nNodes
+        % Read node
+        node = blob{node_index};
+        % Pad
+        nFrequencies_in = size(node, 2);
+        nFrequencies_out = ceil(nFrequencies_in / B);
+        padding_length = nFrequencies_out * B - nFrequencies_in;
+        padding = zeros(size(node, 1), padding_length);
+        node = cat(2, node, padding);
+        % Reshape
+        node = reshape(node, size(node, 1), B, nFrequencies_out);
+        % Max-pool
+        node = max(node, [], 2);
+        % Write node
+        blob{node_index} = squeeze(node);
+        % Update ranges
+        S{1+2}.ranges{1+0}{blob_index}{node_index}(2,2) = B;
+    end
+    % Write blob
+    S{1+2}.data{blob_index} = blob;
+end
+end

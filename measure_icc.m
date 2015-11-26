a8c2208b4afe285ad3bd05153b5bf81cc45c5bc3
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-26 17:39:04 +0100

    write measure_icc function

---
+++ measure_icc.m
@@ -0,0 +1,21 @@
+function icc = measure_icc(setting, features)
+%% Compute the centroid of each instrument
+nFeatures = size(features(1).data, 1);
+nInstruments = length(unique([features.instrument_id]));
+instruments = cell(1, nInstruments);
+centroids = zeros(nFeatures, nInstruments);
+for instrument_id = 1:nInstruments
+    instrument = features([features.instrument_id] == instrument_id);
+    centroids(:, instrument_id) = mean([instrument.data], 2);
+    instruments{instrument_id} = instrument;
+end
+
+%% Compute the inter-class variance
+interclass_variance = sum(var(centroids, [], 2));
+
+%% Compute the variance across samples
+global_variance = sum(var([features.data], [], 2));
+
+%% Return Fisher's Intra-class Correlation Coefficient (ICC)
+icc = interclass_variance / global_variance;
+end

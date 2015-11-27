9f1be853d2cb94b29c29b823236062b98d07bc25
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-27 10:08:30 +0100

    update measure_icc with log scattering coefficients

--- measure_icc.m
+++ measure_icc.m
@@ -1,21 +1,30 @@
 function icc = measure_icc(features)
-%% Compute the centroid of each instrument
-nFeatures = size(features(1).data, 1);
-nInstruments = length(unique([features.instrument_id]));
-instruments = cell(1, nInstruments);
-centroids = zeros(nFeatures, nInstruments);
-for instrument_id = 1:nInstruments
-    instrument = features([features.instrument_id] == instrument_id);
-    centroids(:, instrument_id) = mean([instrument.data], 2);
-    instruments{instrument_id} = instrument;
+mus = logspace(-6,6);
+nMus = length(mus);
+icc = zeros(1, nMus);
+
+for mu_index = 1:nMus
+    %% Load log-features
+    [features.logdata] = log1p([features.data] / mu);
+    
+    %% Compute the centroid of each instrument
+    nFeatures = size(features(1).data, 1);
+    nInstruments = length(unique([features.instrument_id]));
+    instruments = cell(1, nInstruments);
+    centroids = zeros(nFeatures, nInstruments);
+    for instrument_id = 1:nInstruments
+        instrument = features([features.instrument_id] == instrument_id);
+        centroids(:, instrument_id) = mean([instrument.logdata], 2);
+        instruments{instrument_id} = instrument;
+    end
+    
+    %% Compute the inter-class variance
+    interclass_variance = sum(var(centroids, [], 2));
+    
+    %% Compute the variance across samples
+    global_variance = sum(var([features.logdata], [], 2));
+    
+    %% Return Fisher's Intra-class Correlation Coefficient (ICC)
+    icc(mu_index) = interclass_variance / global_variance;
 end
-
-%% Compute the inter-class variance
-interclass_variance = sum(var(centroids, [], 2));
-
-%% Compute the variance across samples
-global_variance = sum(var([features.data], [], 2));
-
-%% Return Fisher's Intra-class Correlation Coefficient (ICC)
-icc = interclass_variance / global_variance;
 end

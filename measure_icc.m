fd10383c0846fcefd2f295c0e6d53e22f7183141
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-28 00:32:58 +0100

    add plain ICC to measure_icc

--- measure_icc.m
+++ measure_icc.m
@@ -1,5 +1,29 @@
 function icc = measure_icc(features)
-mus = logspace(-10, -3, 8);
+%% Plain ICC
+nFiles = length(features);
+nFeatures = size(features(1).data, 1);
+nInstruments = length(unique([features.instrument_id]));
+
+% Compute the centroid of each instrument
+instruments = cell(1, nInstruments);
+centroids = zeros(nFeatures, nInstruments);
+for instrument_id = 1:nInstruments
+    instrument = features([features.instrument_id] == instrument_id);
+    centroids(:, instrument_id) = mean([instrument.data], 2);
+    instruments{instrument_id} = instrument;
+end
+
+% Compute the inter-class variance
+interclass_variance = sum(var(centroids, [], 2));
+
+% Compute the variance across samples
+global_variance = sum(var([features.data], [], 2));
+
+% Return Fisher's Intra-class Correlation Coefficient (ICC)
+plain_icc = interclass_variance / global_variance;
+
+%% Uniform log ICCs
+mus = logspace(-15, -1, 15);
 nMus = length(mus);
 uniformlog_iccs = zeros(1, nMus);
 nFiles = length(features);
@@ -33,7 +57,8 @@ for mu_index = 1:nMus
     uniformlog_iccs(mu_index) = interclass_variance / global_variance;
 end
 
-etas = logspace(-2, 2, 9);
+%% Median-based log ICCs
+etas = logspace(-15, -1, 15);
 nEtas = length(etas);
 medianlog_iccs = zeros(1, nEtas);
 feature_medians = max(median(max([features.data], 0), 2), eps());
@@ -69,7 +94,9 @@ for eta_index = 1:nEtas
     medianlog_iccs(eta_index) = interclass_variance / global_variance;
 end
 
+icc.plain_icc = plain_icc;
 icc.uniformlog_iccs = uniformlog_iccs;
 icc.medianlog_iccs = medianlog_iccs;
-icc.best_icc = max(max(uniformlog_iccs), max(medianlog_iccs));
+icc.best_icc = ...
+    max([plain_icc, max(uniformlog_iccs), max(medianlog_iccs)]);
 end
